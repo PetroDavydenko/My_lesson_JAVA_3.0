96efff959a16907d9f0295541261c94431fca818
Admin <petrodavidenko32@gmail.com>
2022-03-14 22:38:30 +0200

    Initial commit

---
+++ src/test_3.java
@@ -0,0 +1,20 @@
+public class test_3 {
+    public static void main (String args[]){
+        byte number1 = 20;
+        short number2 = 100;
+        long number3 = 10000000;
+        String title = "JAVA найкраща";
+        char letter = 'M';
+        double sum = 100.000;
+        float pi = 3.14f;
+        boolean resoult = true;
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(title);
+        System.out.println(letter);
+        System.out.println(sum);
+        System.out.println(pi);
+        System.out.println(resoult);
+    }
+}
